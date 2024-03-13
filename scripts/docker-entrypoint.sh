5c75a9798d0cbdae02d27f680a7e08da0c9499c9
Giorgi Abashidze <giorgi.abashidze2@gmail.com>
2024-03-13 15:34:48 +0400

    feat: create base django application

---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+echo "Check postgres service availability"
+python ~/check_service.py --service-name "${DATABASE_NAME}" --ip "${DATABASE_HOST}" --port "${DATABASE_PORT}"
+
+echo "Apply database migrations"
+python src/manage.py migrate --noinput
+
+echo "Collecting static files"
+python src/manage.py collectstatic --noinput
+
+echo "Running app with gunicorn"
+
+gunicorn --env DJANGO_SETTINGS_MODULE="${DJANGO_SETTINGS_MODULE}" --user api --bind 0.0.0.0:8000  core.wsgi
+
+exec "$@"
---
+++ src/core/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # API Schema URLs
+    path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="api-schema"),
+        name="api-docs",
+    ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
